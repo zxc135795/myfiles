945d68d92e90c944022ae33ebb17274852725681
zxc135795 <2388446799@qq.com>
2020-02-14 16:39:34 +0800

    属性增删 book，hero增加，删除

--- end/demo001/bookdemo/booktest/urls.py
+++ end/demo001/bookdemo/booktest/urls.py
@@ -9,11 +9,15 @@
 from django.conf.urls import url
 
 from . import views
+
 app_name = "booktest"
 urlpatterns = [
 
     url(r'^$', views.index, name='index'),
     url(r'^list/$', views.list, name='list'),
-    url(r'detail/(\d+)/', views.detail, name='detail')
+    url(r'^detail/(\d+)/$', views.detail, name='detail'),
+    url(r'^deletebook/(\d+)/$', views.deletebook, name='deletebook'),
+    url(r'^deletehero/(\d+)/$', views.deletehero, name='deletehero'),
+    url(r'^addhero/(\d+)/$', views.addhero, name='addhero'),
 
 ]
--- end/demo001/bookdemo/booktest/views.py
+++ end/demo001/bookdemo/booktest/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 # 视图数据请求
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Hero, Book
 
@@ -23,6 +23,37 @@ def detail(request, bookid):
     # result = template.render(context)
     # return HttpResponse(result)
     return render(request, 'detail.html', {'book': book})
+
+
+def deletebook(request, bookid):
+    # 删除处理
+    book = Book.objects.get(id=bookid)
+    book.delete()
+    url = reverse('booktest:index')
+    return redirect(to=url)
+
+
+def deletehero(request, heroid):
+    hero = Hero.objects.get(id=heroid)
+    bookid = hero.book.id
+    hero.delete()
+    url = reverse('booktest:detail', args=(bookid,))  # args 里为元组 （'a',)
+    return redirect(to=url)
+
+
+def addhero(request, bookid):
+    if request.method == 'GET':
+        return render(request, 'addhero.html')
+    elif request.method == 'POST':
+        hero = Hero()
+        hero.name = request.POST.get('heroname')
+        hero.content = request.POST.get('content')
+        hero.gender = request.POST.get('sex')
+        hero.book = Book.objects.get(id=bookid)
+        hero.save()
+        url = reverse('booktest:detail', args=(bookid,))
+        return redirect(to=url)
+
 
 
 def list(request):
